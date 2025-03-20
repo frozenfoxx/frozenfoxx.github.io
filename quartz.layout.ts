e63895e3d9287eb6f60137f2b2b6e79964e53e0b
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2025-03-20 15:07:47 -0700

    Added comments

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'frozenfoxx/frozenfoxx.github.io',
+        // from data-repo-id
+        repoId: 'R_kgDON3GDxA',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDON3GDxM4CoSwG',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       "GitHub": "https://github.com/frozenfoxx",
